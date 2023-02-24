4e016cdd509bd38ae7b9aaee15d0f62c30ecefdf
Andrew Berezovskyi <andriib@kth.se>
2023-02-24 15:16:58 +0100

    Split the E2E tests to tell apart different server failures
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- bin/test-jazz_nordic.sh
+++ bin/test-jazz_nordic.sh
@@ -10,9 +10,6 @@ pushd "$(dirname "$0")/../lyo-client-samples"
 mvn -B clean compile
 
 # EWM
-mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.EWMSample" -Dexec.args="-url https://jazz.net/sandbox02-ccm/ \
-  -user ${JAZZ_NET_USERNAME} -password \"${JAZZ_NET_PASSWORD}\" \
-  -project \"smarx721 Project (Change and Architecture Management)\" --basic"
 mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.EWMSample" -Dexec.args="-url https://nordic.clm.ibmcloud.com/ccm/ \
   -user ${JAZZ_NORDIC_USERNAME} -password \"${JAZZ_NORDIC_PASSWORD}\" \
   -project \"OSLC Open Environment (EWM)\""
@@ -21,15 +18,8 @@ mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.EWMSample" -De
     -project \"JKE Banking (Change Management)\""
 
 # ERM
-# FIXME: add -basic auth support to ERM sample
-#mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.ERMSample" -Dexec.args="-url https://jazz.net/sandbox02-rm/ \
-#  -user ${JAZZ_NET_USERNAME} -password \"${JAZZ_NET_PASSWORD}\" \
-#  -project \"smarx721 Project (Requirements Management)\" --basic"
 mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.ERMSample" -Dexec.args="-url https://nordic.clm.ibmcloud.com/rm/ \
   -user ${JAZZ_NORDIC_USERNAME} -password \"${JAZZ_NORDIC_PASSWORD}\" \
   -project \"OSLC Open Environment (DNG)\""
-mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.ERMSample" -Dexec.args="-url https://oslc.itm.kth.se:9443/rm/ \
-    -user ${JAZZ_ITM_USERNAME} -password \"${JAZZ_ITM_PASSWORD}\" \
-    -project \"JKE Banking (Requirements Management)\""
 
 popd
