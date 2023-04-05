a3b5a285823d7fa8d1801a2d2b9ca8065d8a2184
Andrew Berezovskyi <andriib@kth.se>
2023-04-05 12:59:02 +0200

    update KTH URI base
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- bin/test-jazz_kth.sh
+++ bin/test-jazz_kth.sh
@@ -10,12 +10,12 @@ pushd "$(dirname "$0")/../lyo-client-samples"
 mvn -B clean compile
 
 # EWM
-mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.EWMSample" -Dexec.args="-url https://oslc.itm.kth.se:9443/ccm/ \
+mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.EWMSample" -Dexec.args="-url https://oslc.itm.kth.se/ccm/ \
     -user ${JAZZ_ITM_USERNAME} -password \"${JAZZ_ITM_PASSWORD}\" \
     -project \"JKE Banking (Change Management)\""
 
 # ERM
-mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.ERMSample" -Dexec.args="-url https://oslc.itm.kth.se:9443/rm/ \
+mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.ERMSample" -Dexec.args="-url https://oslc.itm.kth.se/rm/ \
     -user ${JAZZ_ITM_USERNAME} -password \"${JAZZ_ITM_PASSWORD}\" \
     -project \"JKE Banking (Requirements Management)\""
 
