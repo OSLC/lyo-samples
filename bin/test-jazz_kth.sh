2f5d34991a4186f17e898992cf131ac3ba94a974
Andrew Berezovskyi <andriib@kth.se>
2023-07-16 20:50:44 +0200

    build: re-enable smoke tests on KTH ELM install
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- bin/test-jazz_kth.sh
+++ bin/test-jazz_kth.sh
@@ -12,11 +12,11 @@ mvn -B clean compile
 # EWM
 mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.EWMSample" -Dexec.args="-url https://oslc.itm.kth.se/ccm/ \
     -user ${JAZZ_ITM_USERNAME} -password \"${JAZZ_ITM_PASSWORD}\" \
-    -project \"JKE Banking (Change Management)\""
+    -project \"Lyo Smoke Test Lifecycle Project (Change Management)\""
 
 # ERM
 mvn -B exec:java -Dexec.mainClass="org.eclipse.lyo.samples.client.ERMSample" -Dexec.args="-url https://oslc.itm.kth.se/rm/ \
     -user ${JAZZ_ITM_USERNAME} -password \"${JAZZ_ITM_PASSWORD}\" \
-    -project \"JKE Banking (Requirements Management)\""
+    -project \"Lyo Smoke Test Lifecycle Project (Requirements)\""
 
 popd
