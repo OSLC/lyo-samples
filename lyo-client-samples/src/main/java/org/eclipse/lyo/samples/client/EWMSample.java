9d3ed5cb21cddf9a3e3968ac768af3f9a45ee7eb
Andrew Berezovskyi <andrew@berezovskyi.me>
2024-11-12 22:52:54 +0100

    fix: import

--- lyo-client-samples/src/main/java/org/eclipse/lyo/samples/client/EWMSample.java
+++ lyo-client-samples/src/main/java/org/eclipse/lyo/samples/client/EWMSample.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.client.ClientBuilder;
