67221c9aa0b4a5a24240787ce20bc78e498d1072
Andrew Berezovskyi <andriib@kth.se>
2025-06-13 21:09:15 +0200

    fix: CORS filter code

--- client-oauth-discovery-dui/src/main/java/org/eclipse/lyo/samples/client/OslcCorsFilter.java
+++ client-oauth-discovery-dui/src/main/java/org/eclipse/lyo/samples/client/OslcCorsFilter.java
@@ -1,9 +1,9 @@
-package org.eclipse.lyo.oslc4j.client.samples;
+package org.eclipse.lyo.samples.client;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.ext.Provider;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
 /**
