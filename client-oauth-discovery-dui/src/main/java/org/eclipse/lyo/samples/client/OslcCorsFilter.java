d43efb59d58b7865ecbfa4c048f0074ec23cae96
Andrew Berezovskyi <andriib@kth.se>
2025-06-14 13:52:38 +0200

    chore: reformat

--- client-oauth-discovery-dui/src/main/java/org/eclipse/lyo/samples/client/OslcCorsFilter.java
+++ client-oauth-discovery-dui/src/main/java/org/eclipse/lyo/samples/client/OslcCorsFilter.java
@@ -13,23 +13,25 @@ import java.io.IOException;
 public class OslcCorsFilter implements ContainerResponseFilter {
 
     @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
+    public void filter(
+            ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
         String originAllow = "*";
         final String requestOrigin = requestContext.getHeaderString("Origin");
         if (requestOrigin != null) {
-            //needed to enable Access-Control-Allow-Credentials
+            // needed to enable Access-Control-Allow-Credentials
             originAllow = requestOrigin;
         }
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Origin", originAllow);
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization, oslc-core-version, Configuration-Context, OSLC-Configuration-Context");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", originAllow);
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext
+                .getHeaders()
+                .add(
+                        "Access-Control-Allow-Headers",
+                        "origin, content-type, accept, authorization, oslc-core-version,"
+                                + " Configuration-Context, OSLC-Configuration-Context");
+        responseContext
+                .getHeaders()
+                .add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
